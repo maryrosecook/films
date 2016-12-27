f4acfe22c8875792e4a1612ddc323bdb1c1de1b8
Mary Rose Cook <mary@maryrosecook.com>
2016-12-27 08:02:21 +0000

    DRY up rich mix tests

--- spec/rich-mix.spec.js
+++ spec/rich-mix.spec.js
@@ -4,6 +4,7 @@ let requestPromise = require("request-promise");
 let moment = require("moment");
 let fs = require("fs-extra");
 let path = require("path");
+let _ = require("underscore");
 
 let richMix = require("../src/listing-sources/rich-mix");
 let listing = require("../src/listing");
@@ -16,24 +17,28 @@ describe("scraping rich mix", function() {
     });
   });
 
-  it("gets first time of first film", function() {
-    var pageContent = fs.readFileSync(
-      path.join(__dirname, "./pages/richmix.org.uk.html"),
-      "utf8");
-    expect(richMix.listings(pageContent, listing)[0])
-      .toEqual(listing(new Date("2016-12-20T11:00:00+00:00"),
-                       "Rogue One: A Star Wars Story 2D",
-                       "Rich Mix"));
-  });
+  describe("#listings", function() {
+    let listings;
+    beforeEach(function() {
+      let pageContent = fs.readFileSync(
+        path.join(__dirname, "./pages/richmix.org.uk.html"),
+        "utf8");
+      listings = richMix.listings(pageContent, listing);
+    });
 
-  it("gets last time of last film", function() {
-    var pageContent = fs.readFileSync(
-      path.join(__dirname, "./pages/richmix.org.uk.html"),
-      "utf8");
-    var listings = richMix.listings(pageContent, listing);
-    expect(listings[listings.length - 1])
-      .toEqual(listing(new Date("2017-02-16T19:00:00+00:00"),
-                       "NT Live: Saint Joan",
-                       "Rich Mix"));
+    it("gets first time of first film", function() {
+      expect(listings[0])
+        .toEqual(listing(new Date("2016-12-20T11:00:00+00:00"),
+                         "Rogue One: A Star Wars Story 2D",
+                         "Rich Mix"));
+    });
+
+    it("gets last time of last film", function() {
+      expect(_.last(listings))
+        .toEqual(listing(new Date("2017-02-16T19:00:00+00:00"),
+                         "NT Live: Saint Joan",
+                         "Rich Mix"));
+    });
+
   });
 });
