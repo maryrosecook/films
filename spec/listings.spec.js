95f1ece83d95a3cf1a1dfd753bfdb84165c6839b
Mary Rose Cook <mary@maryrosecook.com>
2017-01-13 16:02:51 +0000

    Test groupByDateAndFilm()

--- spec/listings.spec.js
+++ spec/listings.spec.js
@@ -20,4 +20,53 @@ describe("listings", function() {
                                            jsonObjects[0].cinema);
     });
   });
+
+  describe("#groupByDateAndFilm", function() {
+    it("groups by date and then film", function() {
+      let date = moment();
+      let listingObjects = [{
+        dateTime: date,
+        film: "Margaret",
+        cinema: "Rich Mix"
+      }];
+
+      let dates = listings
+          .groupByDateAndFilm(listingObjects);
+
+      expect(dates).toEqual([{
+        date: date.format("YYYY-MM-DD"),
+        films: [{
+          film: "Margaret",
+          listings: [{
+            dateTime: date,
+            film: "Margaret",
+            cinema: "Rich Mix"
+          }]
+        }]
+      }])
+    });
+
+    it("groups films by date", function() {
+      let date1 = moment();
+      let date2 = moment().add(1, "day");
+      let listingObjects = [{
+        dateTime: date1,
+        film: "Margaret",
+        cinema: "Rich Mix"
+      }, {
+        dateTime: date2,
+        film: "Heat",
+        cinema: "Rich Mix"
+      }];
+
+      let dates = listings
+          .groupByDateAndFilm(listingObjects);
+
+      expect(dates[0].date).toEqual(date1.format("YYYY-MM-DD"));
+      expect(dates[0].films[0].film).toEqual("Margaret");
+
+      expect(dates[1].date).toEqual(date2.format("YYYY-MM-DD"));
+      expect(dates[1].films[0].film).toEqual("Heat");
+    });
+  });
 });
