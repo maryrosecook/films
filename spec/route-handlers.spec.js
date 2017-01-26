6b96b42df4657e215ff58645b8e9000da6da9091
Mary Rose Cook <mary@maryrosecook.com>
2017-01-26 18:49:04 +0000

    Add tests for index page

--- spec/route-handlers.spec.js
+++ spec/route-handlers.spec.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let cheerio = require("cheerio");
+let sinon = require("sinon");
+require("sinon-as-promised"); // adds promise methods to sinon
 
 let proxyquire = require("proxyquire").noPreserveCache();
 let listing = require("../src/listing");
@@ -41,6 +42,28 @@ describe("routeHandlers", function() {
       expect(db.read.calls.argsFor(0)[0]).toEqual("data");
       expect(responseMock.json)
         .toHaveBeenCalledWith(listings);
+    });
+  });
+
+  describe("index", function() {
+    it("sets timezone", function() {
+      let setDefault = sinon.stub();
+      let routeHandlers = proxyquire("../src/route-handlers", {
+        "moment-timezone": { tz: { setDefault: setDefault } }
+      });
+
+      routeHandlers.index(requestMock, responseMock);
+      expect(setDefault.firstCall.args[0]).toEqual("Europe/London");
+    });
+
+    it("responds with something (anything at all)", function() {
+      let routeHandlers = require("../src/route-handlers");
+
+      routeHandlers.index(requestMock, responseMock);
+
+      expect(typeof responseMock.send.calls.argsFor(0)[0] ===
+             "string")
+        .toEqual(true);
     });
   });
 });
