a226d5872669d2586285cb27b89bbe55227ecbb5
Mary Rose Cook <mary@maryrosecook.com>
2017-01-27 15:55:11 +0000

    Add basic youtube trailer finder

---
+++ spec/youtube.spec.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let sinon = require("sinon");
+require("sinon-as-promised"); // adds promise methods to sinon
+
+let proxyquire = require("proxyquire").noPreserveCache();
+
+describe("#youtube", function() {
+  it("sets key on youtube instance", function(done) {
+    let setKey = sinon.stub();
+    let youtube = proxyquire("../src/youtube", {
+      "youtube-node": sinon.stub().returns({
+        setKey: setKey,
+        search: (_1, _2, cb) => {
+          // force search to abort - only care about setKey
+          // being called, which has already happened
+          cb("error");
+        }
+      })
+    });
+
+    youtube().catch(() => {
+      expect(setKey.firstCall.args[0].length > 0).toEqual(true)
+      done();
+    });
+  });
+
+  it("gives single url for a query", function(done) {
+    let response = { items: [{ id: { videoId: '2GfZl4kuVNI' } }] };
+
+    let youtube = proxyquire("../src/youtube", {
+      "youtube-node": sinon.stub().returns({
+        setKey: sinon.stub(),
+        search: (query, maxResults, cb) => {
+          expect(query).toEqual("heat trailer");
+          expect(maxResults).toEqual(1);
+
+          cb(null, response);
+        }
+      })
+    });
+
+    youtube("heat trailer")
+      .then((result) => {
+        expect(result)
+          .toEqual("https://www.youtube.com/watch?v=2GfZl4kuVNI");
+        done();
+      })
+  });
+});
---
+++ src/youtube.js
@@ -0,0 +1,38 @@
+"use strict";
+
+require('dotenv').config();
+var YouTube = require('youtube-node');
+
+function youtube(query) {
+  return new Promise(function(resolve, reject) {
+    authenticatedInstance()
+      .search(query, 1, function(err, response) {
+        if (err || responseSearchResults(response).length === 0) {
+          reject("Couldn't get trailer");
+        }
+
+        resolve(youtubeUrlFromVideoId(
+          searchResultVideoId(responseSearchResults(response)[0])));
+      });
+  });
+};
+
+function responseSearchResults(response) {
+  return response.items;
+};
+
+function searchResultVideoId(result) {
+  return result.id.videoId;
+};
+
+function youtubeUrlFromVideoId(videoId) {
+  return `https://www.youtube.com/watch?v=${videoId}`;
+};
+
+function authenticatedInstance() {
+  let youTube = new YouTube();
+  youTube.setKey(process.env.YOUTUBE_API);
+  return youTube;
+};
+
+module.exports = youtube;
