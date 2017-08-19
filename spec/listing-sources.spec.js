ec0e3066881286ef7e1de7bb5e84d1e487477b0e
Mary Rose Cook <mary@maryrosecook.com>
2017-08-19 16:54:10 +0100

    Make listing sources test check for actual modules

--- spec/listing-sources.spec.js
+++ spec/listing-sources.spec.js
@@ -3,11 +3,28 @@
 let listingSources = require("../src/listing-sources");
 
 describe("#listingSources", function() {
-  it("returns array listingSources", function() {
-    expect(listingSources().length > 0).toEqual(true);
+  let sources;
+  beforeAll(() => {
+    sources = listingSources();
+  });
 
-    listingSources().forEach(function(listingSource) {
-      expect(listingSource.listings).toBeDefined();
-    });
+  fit("includes hackney picturehouse", function() {
+    expect(sources[0])
+      .toEqual(require("../src/listing-sources/hackney-picturehouse"));
+  });
+
+  fit("includes rich mix", function() {
+    expect(sources[1])
+      .toEqual(require("../src/listing-sources/rich-mix"));
+  });
+
+  fit("includes rio", function() {
+    expect(sources[2])
+      .toEqual(require("../src/listing-sources/rio"));
+  });
+
+  fit("includes britinfo cinemas", function() {
+    expect(sources[3])
+      .toEqual(require("../src/listing-sources/britinfo-cinemas"));
   });
 });
