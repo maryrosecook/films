5845b564d7b8132f022dfaf8a7446581151de0e0
Mary Rose Cook <mary@maryrosecook.com>
2017-08-19 17:06:39 +0100

    Automatically get listing sources from listing-sources dir

--- spec/listing-sources.spec.js
+++ spec/listing-sources.spec.js
@@ -8,23 +8,23 @@ describe("#listingSources", function() {
     sources = listingSources();
   });
 
-  fit("includes hackney picturehouse", function() {
+  fit("includes britinfo cinemas", function() {
     expect(sources[0])
+      .toEqual(require("../src/listing-sources/britinfo-cinemas"));
+  });
+
+  fit("includes hackney picturehouse", function() {
+    expect(sources[1])
       .toEqual(require("../src/listing-sources/hackney-picturehouse"));
   });
 
   fit("includes rich mix", function() {
-    expect(sources[1])
+    expect(sources[2])
       .toEqual(require("../src/listing-sources/rich-mix"));
   });
 
   fit("includes rio", function() {
-    expect(sources[2])
-      .toEqual(require("../src/listing-sources/rio"));
-  });
-
-  fit("includes britinfo cinemas", function() {
     expect(sources[3])
-      .toEqual(require("../src/listing-sources/britinfo-cinemas"));
+      .toEqual(require("../src/listing-sources/rio"));
   });
 });
--- src/listing-sources.js
+++ src/listing-sources.js
@@ -6,19 +6,17 @@ let path = require("path");
 const LISTING_SOURCES_PATH = path.join(__dirname,
                                        "./listing-sources/");
 
-const LISTING_SOURCE_FILENAMES = [
-  "hackney-picturehouse.js",
-  "rich-mix.js",
-  "rio.js",
-  "britinfo-cinemas.js"
-];
-
 function listingSources() {
-  return LISTING_SOURCE_FILENAMES
-    .map((listingSourceFilename) => {
-      return require(path.join(LISTING_SOURCES_PATH,
-                               listingSourceFilename));
-    });
+  return listingSourcesNames()
+    .map(name => {
+      return path.join(LISTING_SOURCES_PATH, name)
+    })
+    .map(require);
+};
+
+function listingSourcesNames() {
+  return fs.readdirSync(LISTING_SOURCES_PATH)
+    .filter(name => { return name.includes(".js") })
 };
 
 module.exports = listingSources;
